ee6ced5ecd8f5e29c0dd1706097aecdc5f85f44c
MGeordie <geomolloyuni@gmail.com>
2022-07-08 17:56:03 +1000

    Minesweepr working with UI. Still needs many UI adjustments.

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -42,17 +42,12 @@ import random
 
 #Variables
 mineField = pd.DataFrame()
-
-#Generate Window
-# window = tk.Tk()
-# window.title("Minesweeper")
-# window = tk.Label(text="Tkinter is cool.", width=80, height=25)
-# window.pack()
-# window.mainloop()
+buttonDict = {}
+mineClose = 0
+# mineCount = 0
 
 def genMines(difficulty):
     if difficulty == "Easy":
-        mineCount = 0
         print("Easy mines being generated...")
         for i in range(0,5):
             for y in range(0,5):
@@ -63,7 +58,6 @@ def genMines(difficulty):
                 else:
                     print("Made it to Mine at",i,y)
                     mineField.at[i,y] = "Mine"
-                    mineCount =+ 1
     elif difficulty == "Medium":
         print("Medium mines being generated...")
     elif difficulty =="Hard":
@@ -72,22 +66,73 @@ def genMines(difficulty):
         print("Problem with difficulty...")
     return mineField
 
-def checkMine(test,x,y):
+def checkMine(q,x,y):
     if mineField.loc[x,y] == "Mine":
         print("Boom")
+        buttonDict[q].config(text="Boom")
+        buttonDict[q].config(state="disabled")
     elif mineField.loc[x,y] =="Safe":
         print("Safe")
-        test = "X"
+        mineClose = checkSurround(q,x,y)
+        buttonDict[q].config(text = mineClose)
+        buttonDict[q].config(state="disabled")
     else:
         print("Something wrong in checkMine")
+
+def checkSurround(q,x,y):
+    minRowRange = 0
+    maxRowRange = 3
+    minColRange = 0
+    maxColRange = 3
+    mineClose = 0
+
+    print(x,y)
+    print(len(mineField))
+    print(len(mineField.columns))
+
+    row = x-1
+    col = y-1
+    #Check Border
+    if x-1 < 0:
+        minRowRange = 1
+        print("At the top")
+    if y-1 < 0:
+        minColRange = 1
+        print("At the left edge")
+    if x+1 > len(mineField) - 1:
+        maxRowRange = 2
+        print("At the bottom")
+    if y+1 > len(mineField.columns) -1:
+        maxColRange = 2
+        print("At the right edge")
+
+    for i in range(minRowRange,maxRowRange):
+        row = x - 1
+        row = row + i
+        col = y-1
+        for a in range(minColRange,maxColRange):
+            col = col + a
+            if mineField.loc[row,col] == "Mine":
+                mineClose = mineClose + 1
+            col = y-1
+    return mineClose
+
 
 
 def genGame(mineField):
     window = tk.Tk()
-    test= "O"
+    window.geometry("800x1000")
+    window.title("Minesweeper")
+    i=0
     for x in mineField:
         for y in mineField:
-            tk.Button(textvariable=test, command = lambda row=x ,column=y: checkMine(test,row,column)).grid(row=x,column=y,sticky="ew")
+            buttonName = tk.Button(window, text="", command = lambda n=i, row=x ,column=y: checkMine(n,row,column))
+            buttonName.config(height=3,width=3)
+            buttonName.grid(row=x,column=y,sticky="NSEW")
+            buttonDict[i] = buttonName
+            i=i+1
+
+    lbl = tk.Label(window,text=("Mines Remaining:"))
     window.mainloop()
 
 difficulty = input("What difficulty would you like?")
